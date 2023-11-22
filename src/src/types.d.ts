5af1e9c6459fd9eca87de880b3830965976d8547
Velris <velristv@gmail.com>
2023-11-22 10:41:45 +0100

    Delete socket.io, ajout socket.io-client

--- src/src/types.d.ts
+++ src/src/types.d.ts
@@ -27,4 +27,14 @@ export interface SlashCommand {
     execute: (interaction: CommandInteraction) => Promise<void>;
 }
 
+export interface ServerToClientEvents {
+    noArg: () => void;
+    basicEmit: (a: number, b: string, c: Buffer) => void;
+    withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+export interface ClientToServerEvents {
+    hello: () => void;
+}
+
 export {};
