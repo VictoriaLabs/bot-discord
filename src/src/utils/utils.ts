9e86123f5e3e5597ee972d3e9ba406e153b7cffb
Vincent MAGNIEN <vincentmagnienpro@gmail.com>
2023-12-20 23:16:08 +0100

    :recycle: Refactor code

--- src/src/utils/utils.ts
+++
@@ -1,6 +0,0 @@
-// --- Poll --- //
-export function calculatePollTimeLeft(endDate: Date): number {
-  const today = new Date();
-  const diff = endDate.getTime() - today.getTime();
-  return diff;
-}
